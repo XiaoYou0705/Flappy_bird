552a8e5f64b3957f49fb4b085928b0c71182e737
XiaoYou <qweasdzxc930705@gmail.com>
2024-03-28 11:58:16 +0800

    pipe

--- Flappy bird.py
+++ Flappy bird.py
@@ -44,13 +44,40 @@ class Bird(pygame.sprite.Sprite):
         key = pygame.key.get_pressed()
         
         if key[pygame.K_SPACE]:
-            self.speedy = -12
+            self.speedy = -8
             self.rotate(-10)
         self.speedy += 1.2
         if self.speedy >= 6.5:
             self.speedy = 6.5
             
         self.rect.y += self.speedy
+
+# 下_水管
+class Pipe_down(pygame.sprite.Sprite):
+    def __init__(self, exit_high):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pipe_down
+        self.rect = self.image.get_rect()
+        self.rect.left = 630
+        self.rect.top = exit_high + 100
+        self.speedx = 5
+    def update(self):
+        self.rect.x -= self.speedx
+        if self.rect.right == 0:
+            self.kill()
+# 上_水管       
+class Pipe_top(pygame.sprite.Sprite):
+    def __init__(self, exit_high):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pipe_top
+        self.rect = self.image.get_rect()
+        self.rect.left = 630
+        self.rect.bottom = exit_high - 100
+        self.speedx = 5
+    def update(self):
+        self.rect.x -= self.speedx
+        if self.rect.right == 0:
+            self.kill()
         
 # 介面設定
 WIDTH = 630
@@ -65,10 +92,17 @@ fpsclock = pygame.time.Clock()
 
 # 載入圖片
 bird_img = pygame.image.load(os.path.join("./img/bird.png")).convert_alpha()
+pipe_down_img = pygame.image.load(os.path.join("./img/pipe.png")).convert_alpha()
+pipe_img = pygame.transform.rotate(pipe_down_img, 180)
+
 
 # 調整圖片
 bird = pygame.transform.scale(bird_img, (100, 80))
 bird_fly = pygame.transform.rotate(bird, 45)
+
+pipe_top = pygame.transform.scale(pipe_img, (150, 500))
+pipe_down = pygame.transform.scale(pipe_down_img, (150, 500))
+
 # 顏色
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -79,18 +113,48 @@ skyblue = (45, 171, 255)
 
 # 載入背景
 
+
+# 水管數值
+pipe_x = 775
+exit_y = random.randrange(200, 600)
+
 # 加入角色
+
 all_sprite = pygame.sprite.Group()
+pipes = pygame.sprite.Group()
+top_pipe = Pipe_top(exit_y)
+down_pipe = Pipe_down(exit_y)
+pipes.add(top_pipe)
+pipes.add(down_pipe)
 player = Bird()
 all_sprite.add(player)
+all_sprite.add(top_pipe)
+all_sprite.add(down_pipe)
+
 
 # 主程式
 playing = True
 while playing:
-    gameDisplay.fill(skyblue)
     
+    gameDisplay.fill(skyblue)
+    pipe_x -= 5
+    if(pipe_x <= 200):
+        print(exit_y)
+        exit_y = random.randrange(200, 600)
+        next_top_pipe = Pipe_top(exit_y)
+        next_down_pipe = Pipe_down(exit_y)
+        pipes.add(next_down_pipe)
+        pipes.add(next_top_pipe)   
+        all_sprite.add(next_top_pipe)
+        all_sprite.add(next_down_pipe)
+        pipe_x = 775
+
     all_sprite.update()
     all_sprite.draw(gameDisplay)
+    hits = pygame.sprite.spritecollide(player, pipes, False)
+    for hit in hits:
+        playing = False
+        
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             playing = False
